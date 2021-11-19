07ca6128e5fcbff617b63aad059e15d29bc0e99b
Mateusz Ostrowski <mat.ostrowski96@gmail.com>
2021-11-19 21:26:34 +0000

    5.3 added example

--- example.py
+++ example.py
@@ -3,35 +3,37 @@ import numpy as np
 from perceptron import MLP
 
 if __name__ == "__main__":
-    # create a dataset to train a network for the sum operation
-
-    # create a dataset to train a network for the sum operation
+    
+    # create a dataset to train and test a network 
     x_train = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     y_train = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
     # create a Multilayer Perceptron with one hidden layer
-    mlp = MLP(4, [2,4], 4, bias = True)
+    mlp = MLP(4, [2], 4)
 
     # train network
-    x = mlp.train(x_train, y_train, 5000, 0.2, verbose=False)
-    mlp.plot_mse('bias')
+    x = mlp.train(x_train, y_train, 5000, 0.2)
+    # plot a MSE for each outputs and avg output
+    mlp.plot_mse('example_plot')
+    
+    
+    # get first prediction
     x_test = np.array([1, 0, 0, 0])
 
-    # get a prediction
     outputs = mlp.predict(x_test)
     print("Our network believes that is equal to {}".format(outputs))
     x_test = np.array([0, 1, 0, 0])
 
-    # get a prediction
+    # get second prediction
     outputs = mlp.predict(x_test)
     print("Our network believes that is equal to {}".format(outputs))
     x_test = np.array([0, 0, 1, 0])
 
-    # get a prediction
+    # get third prediction
     outputs = mlp.predict(x_test)
     print("Our network believes that is equal to {}".format(outputs))
     x_test = np.array([0, 0, 0, 1])
 
-    # get a prediction
+    # get fourth prediction
     outputs = mlp.predict(x_test)
     print("Our network believes that is equal to {}".format(outputs))
